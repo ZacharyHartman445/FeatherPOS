076e7db420b81bc5e7cc6829a84d05536209c893
ZacharyHartman445 <zhartma445@gmail.com>
2021-10-16 09:57:16 -0500

    Updating MenuItem
    Updating MenuItem is more specific functions. Considering making a base class to reference to that has the shared functions between MenuItem and Ingredients

--- src/main/java/com/example/featherpos/MenuItem.java
+++ src/main/java/com/example/featherpos/MenuItem.java
@@ -5,11 +5,24 @@ public class MenuItem {
     String name;
     String[] IngredientsUsed;
     double cost;
+    double baseCost;
+    int inStock;
+    int sold;
+    double saleCost;
+    double salePriceOff;
+    double salePercentage;
+
     //Constructor
   public MenuItem(){
        name = "N/A";
         IngredientsUsed = new String[]{"None"};
         cost = 0;
+        baseCost = 0;
+        inStock = 0;
+        sold = 0;
+        saleCost = 0;
+        salePriceOff = 0;
+        salePercentage = 0;
    }
 //getter functions
    String getName (){
@@ -21,6 +34,18 @@ public class MenuItem {
    String[] getIngredientsUsed(){
        return this.IngredientsUsed;
    }
+   int getInStock(){
+      return this.inStock;
+   }
+   int getSold(){
+      return this.sold;
+   }
+   double getBaseCost(){
+      return this.baseCost;
+   }
+
+
+
    //setter functions
     void setName (String newName){
        this.name = newName;
@@ -28,9 +53,21 @@ public class MenuItem {
     void setCost (double newCost){
        this.cost = newCost;
     }
+    void setBaseCost(double currentBase){
+      this.baseCost = currentBase;
+    }
     void setIngredientsUsed(String[] newArray){
        this.IngredientsUsed = newArray;
     }
+    void setInStock(int newInStock){
+      this.inStock = newInStock;
+    }
+    void setSold (int newSold){
+      this.sold = newSold;
+    }
+
+    
+
     //modifying functions
 
     //database functions
