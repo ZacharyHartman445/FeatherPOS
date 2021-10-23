8d792100aaaa8c5dfdd1162c34485cbf59892398
ZacharyHartman445 <zhartma445@gmail.com>
2021-10-23 11:42:42 -0500

    Making it work, SQLLite inclusion

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FeatherPOS
  JavaFx based Point of Sale system
+
+Thanks to xerial for his Java SQLLite Library at https://github.com/xerial/sqlite-jdbc
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.featherpos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.featherpos to javafx.fxml;
