6e9ad9969fcfc5f8b2fc24fd3c60c4614e1890da
tmh18590 <tmh18590@ucmo.edu>
2021-11-15 21:10:51 -0600

    Update FeatherController.java

--- src/main/java/com/example/featherpos/FeatherController.java
+++ src/main/java/com/example/featherpos/FeatherController.java
@@ -1,7 +1,10 @@
 package com.example.featherpos;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class FeatherController {
     @FXML
@@ -11,6 +14,18 @@ public class FeatherController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void actionLogin(ActionEvent event) {
+        
+    }
+
 
     public void getController(){
 
